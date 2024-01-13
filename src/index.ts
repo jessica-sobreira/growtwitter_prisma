e14827e46a3de89c1f40f59d4d3c6d22ffc3782b
jessica-sobreira <j.jecristina@gmail.com>
2024-01-13 19:26:39 -0300

    seguidor routes

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import cors from "cors";
 
 import { AuthController } from "./controllers/auth.controllers";
 import { validarAcesso } from "./middlewares/usuario.middleware";
-import { SeguidorController } from "./controllers/seguidores.controller";
 import { ReplyController } from "./controllers/reply.controller";
 import { usuarioRoutes } from "./routes/usuario.routes";
 import { tweetRoutes } from "./routes/tweet.routes";
 import { likeRoutes } from "./routes/like.routes";
+import { seguidorRoutes } from "./routes/seguidor.routes";
 
 const app = express();
 app.use(express.json());
@@ -15,16 +15,13 @@ app.use(cors());
 
 
 const authController = new AuthController();
-const seguidorController = new SeguidorController();
 const replyController = new ReplyController();
 
 app.use("/usuario", usuarioRoutes());
 app.use("/tweet", [validarAcesso], tweetRoutes());
 app.use("/like", [validarAcesso], likeRoutes());
+app.use("/seguidor", [validarAcesso], seguidorRoutes());
 
-//rotas seguidores
-app.post("/seguidor", [validarAcesso], seguidorController.seguirUsuario);
-app.get("/seguidores/:id", [validarAcesso], seguidorController.listarSeguidores);
 
 //rotas reply
 app.post("/reply", [validarAcesso], replyController.criarReply);
---
+++ src/routes/seguidor.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { SeguidorController } from "../controllers/seguidores.controller";
+import { validarAcesso } from "../middlewares/usuario.middleware";
+
+export function seguidorRoutes(){
+    const router = Router({
+        mergeParams: true,
+    });
+
+    const seguidorController = new SeguidorController();
+
+    //rotas seguidores
+    router.post("/seguidor", [validarAcesso], seguidorController.seguirUsuario);
+    router.get("/seguidores/:id", [validarAcesso], seguidorController.listarSeguidores);
+
+    return router;
+
+}
