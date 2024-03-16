301c5ae8790d522d8a92146ebfd22069b5bbb2da
jessica-sobreira <j.jecristina@gmail.com>
2024-03-16 18:25:36 -0300

    atualizando

--- src/middlewares/usuario.middleware.ts
+++ src/middlewares/usuario.middleware.ts
@@ -2,13 +2,11 @@ import { NextFunction, Request, Response } from "express";
 
 export function validarAcesso(req: Request, res: Response, next: NextFunction) {
     try {
-        const token = req.headers.authorization;
-        const id = req.params.id;
 
-        if (!token || !id) {
+        if(!req.headers.authorization) {
             return res.status(401).send({
                 ok: false,
-                message: "Token de autenticação inválido",
+                message: "Token ausente",
             });
         }
 
