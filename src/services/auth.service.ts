7cc97a868979f12b83e240ebf997a1bc279a0c18
jessica-sobreira <j.jecristina@gmail.com>
2024-03-25 20:46:43 -0300

    authService

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,18 @@
+import { randomUUID } from "crypto";
 import repository from "../database/prisma.repository";
+
+
+export interface Result {
+    ok: boolean;
+    message: string;
+    code: number;
+    data?: any;
+}
+
 
 export class AuthService {
 
-    public async login(email: string, senha: string) {
+    public async login(email: string, senha: string): Promise<Result> {
         const usuario = await repository.usuario.findFirst({
             where: {
                 email,
@@ -13,6 +23,40 @@ export class AuthService {
                 nome: true
             }
         })
+
+        if(!usuario) {
+            return {
+                ok: false,
+                message: "Credenciais inválidas",
+                code: 401,
+            }
+    
+        }
+
+        const token = randomUUID()
+        
+        await repository.usuario.update({
+            where: {
+                id: usuario.id
+            },
+            data: {
+                token
+
+            }
+        })
+
+
+        return {
+            ok: true,
+            message: "Login realizado com sucesso",
+            code: 200,
+            data: {
+                id: usuario.id,
+                nome: usuario.nome,
+                token
+            } 
+        }
     } 
+
 
 }
