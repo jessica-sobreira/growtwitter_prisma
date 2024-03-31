331070e9bb1d2939829790c0f93e451bf14ed849
jessica-sobreira <j.jecristina@gmail.com>
2024-03-31 20:15:01 -0300

    auth-service testes

--- tests/services/auth-service.spec.ts
+++ tests/services/auth-service.spec.ts
@@ -1,15 +1,23 @@
 import { AuthService } from "../../src/services/auth.service";
+import { prismaMock } from "../config/prisma.mock";
 
 describe("Testes da classe AuthService", () => {
-
+    
     test("deve retornar falha 401 quando o usuário não existe no banco de dados", async () => {
+
         const authService = new AuthService();
+        prismaMock.usuario.findFirst.mockResolvedValue(null);
+
         const result = await authService.login("email", "senha");
 
-        expect(result.code).toBe(401);
+        expect(result.code).toBe(401);        
         expect(result.ok).toBe(false);
         expect(result.message).toBe("Credenciais inválidas");
     });
 
+    
 
 })
+
+    
+        
