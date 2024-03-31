1eb4b440eeaaa2ea90d977c1e18b7879a32bfa1d
jessica-sobreira <j.jecristina@gmail.com>
2024-03-31 15:12:17 -0300

    teste integrado

---
+++ tests/services/auth-service.test.ts
@@ -0,0 +1,14 @@
+import { AuthService } from "../../src/services/auth.service";
+
+describe("Testes da classe AuthService", () => {
+    test("deve retornar falha 401 quando o usuário não existe no banco de dados", async () => {
+        const authService = new AuthService();
+        const result = await authService.login("email", "senha");
+
+        expect(result.code).toBe(401);
+        expect(result.ok).toBe(false);
+        expect(result.message).toBe("Credenciais inválidas");
+    });
+
+
+})
