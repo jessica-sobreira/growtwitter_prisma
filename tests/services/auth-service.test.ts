e31cdbfbd261f1c779e425887b2eaf7c12561476
jessica-sobreira <j.jecristina@gmail.com>
2024-03-31 15:26:12 -0300

    testando auth-service

--- tests/services/auth-service.test.ts
+++ tests/services/auth-service.test.ts
@@ -3,6 +3,12 @@ import { AuthService } from "../../src/services/auth.service";
 describe("Testes da classe AuthService", () => {
     test("deve retornar falha 401 quando o usuário não existe no banco de dados", async () => {
         const authService = new AuthService();
+        authService.login = jest.fn().mockResolvedValue({
+            code: 401,
+            ok: false,
+            message: "Credenciais inválidas",
+        });
+
         const result = await authService.login("email", "senha");
 
         expect(result.code).toBe(401);
@@ -10,5 +16,20 @@ describe("Testes da classe AuthService", () => {
         expect(result.message).toBe("Credenciais inválidas");
     });
 
+    test("deve retornar 200 quando o login for realizado com sucesso", async () => {
 
-})
+        const authService = new AuthService();
+        authService.login = jest.fn().mockResolvedValue({
+            code: 200,
+            ok: true,
+            message: "Login realizado com sucesso",
+        });
+
+        const result = await authService.login("bart@email.com", "1234");
+
+        
+        expect(result.code).toBe(200);
+        expect(result.ok).toBe(true);
+        expect(result.message).toBe("Login realizado com sucesso");
+    });
+});
